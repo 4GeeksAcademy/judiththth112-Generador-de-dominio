273da9734c79b03144a43f32187211218fac5649
Judith Ramírez <judithramirezpachon@gmail.com>
2025-05-02 09:31:13 +0000

    generador dominio

--- src/app.js
+++ src/app.js
@@ -5,7 +5,20 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+  let pronombre = ['la', 'nuestra', 'tu'];
+  let adj = ['gran', 'pequeña', 'bonita'];
+  let nombre = ['ciudad', 'aldea', 'villa'];
+
+  for (let pronoun of pronombre) {
+    for (let adje of adj) {
+      for (let noun of nombre) {
+        let dominio = pronoun + " " + adje + " " + noun;
+        console.log(dominio);
+      }
+    }
+  }
+  console.log("www." dominio ".es");
 };
+
+
