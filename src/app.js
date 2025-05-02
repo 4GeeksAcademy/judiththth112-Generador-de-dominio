07a521dc1e887d0c379c179f70b035890d1603ac
Judith Ramírez <judithramirezpachon@gmail.com>
2025-05-02 09:45:23 +0000

    generador dominio

--- src/app.js
+++ src/app.js
@@ -1,11 +1,3 @@
-import "bootstrap";
-import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function () {
   let pronombre = ['la', 'nuestra', 'tu'];
   let adj = ['gran', 'pequeña', 'bonita'];
   let nombre = ['ciudad', 'aldea', 'villa'];
@@ -13,12 +5,9 @@ window.onload = function () {
   for (let pronoun of pronombre) {
     for (let adje of adj) {
       for (let noun of nombre) {
-        let dominio = pronoun + " " + adje + " " + noun;
+        let dominio = pronoun + adje + noun;
         console.log("www." + dominio + ".es");
       }
     }
   }
-
-};
-
 
